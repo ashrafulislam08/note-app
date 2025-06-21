311c698a6d2924aa0de819f26aa1fe5a098b4195
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-06-21 09:12:40 +0600

    note schema designed

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,47 @@
 import express, { Application, Request, Response } from "express"
+import { model, Schema } from "mongoose"
 const app: Application = express()
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    content: {
+        type: String,
+        default: "",
+    },
+    category: {
+        type: String,
+        enum: ['personal', 'work', 'study', 'other'],
+        default: 'personal'
+    },
+    pinned: {
+        type: Boolean,
+        default: false
+    }, 
+    tags: {
+        label: { type: String, required: true},
+        color: {type: String, default: 'gray'}
+    }
+})
+
+const Note = model("Note", noteSchema)
+
+app.post("/create-note", async(req: Request, res: Response) => {
+    const myNote = new Note({
+        title: "Learning Mongoose",
+        content: "I am learning mongoose"
+    })
+    await myNote.save();
+    res.status(201).json({
+        success: true,
+        message: "Successfully created a note",
+        note: myNote
+    })
+})
+
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!")
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
 import { Server } from "http";
 import app from "./app";
 
@@ -8,6 +7,7 @@ let PORT = 5000
 
 async function main() {
     try{
+        await mongoose.connect("mongodb+srv://note_app:mZhdpWULXdlZWtu3@cluster0.fqfy4uw.mongodb.net/note_app?retryWrites=true&w=majority&appName=Cluster0")
         server = app.listen(PORT, () => {
             console.log(`Server is listening on port ${PORT}`)
         })
