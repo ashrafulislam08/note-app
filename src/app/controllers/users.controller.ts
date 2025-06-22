a7b2a083c9f6f388977908ccee3a7cebe799ecbc
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-06-22 09:39:51 +0600

    users controller created

---
+++ src/app/controllers/users.controller.ts
@@ -0,0 +1,58 @@
+import express, { Request, Response } from "express";
+import { User } from "../models/user.model";
+
+export const usersRoutes = express.Router();
+
+
+usersRoutes.post("/create-user", async(req: Request, res: Response) => {
+    const body = req.body;
+    const user = await User.create(body);
+    res.status(201).json({
+        message: "Successfully created user",
+        success: true,
+        user
+    })
+
+})
+
+usersRoutes.get("/", async (req: Request, res: Response) => {
+    const users = await User.find();
+    res.json({
+        success: true,
+        message: "All users retrieve successful",
+        users
+    })
+})
+
+usersRoutes.get("/:userId", async(req: Request, res: Response) => {
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+    res.status(201).json({
+        success: true,
+        message: "User retrieve successfully",
+        user
+    })
+})
+
+
+usersRoutes.patch("/:userId", async(req: Request, res: Response) => {
+    const userId = req.params.id;
+    const body = req.body;
+    const user = await User.findByIdAndUpdate(userId, body)
+    res.status(201).json({
+        success: true,
+        message: "user updated successfully",
+        user
+    })
+})
+
+
+usersRoutes.delete("/:userId", async(req: Request, res: Response) => {
+    const userId = req.params.id;
+    const deletedUser = User.findByIdAndDelete(userId);
+    res.status(201).json({
+        success: true,
+        message: "user deleted successfully",
+        user: deletedUser
+    })
+})
