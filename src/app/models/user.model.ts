287a6fb12f4bf7707e77d9001a01b42f9b679a6b
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-07-03 14:29:58 +0600

    validated user schema

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,31 +2,42 @@ import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new Schema<IUser>({
-    firstName: {
-        type: String,
-        required: true,
-        trim: true,
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: [3, "Name must be at least 3 characters"],
+    maxLength: 20,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: [3, "Name must be at least 3 characters"],
+    maxLength: 20,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+        validator: function(value) {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+        }
     },
-    lastName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user'
-    }
-
-})
+    message: props => `${props.value} is not a valid email`
+  },
+  password: {
+    type: String,
+    required: true,
+    min: [6, "Password must be at least 6 characters"],
+    max: 50,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+});
 
 export const User = model("User", userSchema);
